249749f0e94a6695ab6afbb609e6201c23d2dfd8
Luciano Oliveira <luciano101@msn.com>
2017-08-09 17:32:04 -0700

    Front end not loading app.css

--- backend/api/billingCycle/billingCycle.js
+++ backend/api/billingCycle/billingCycle.js
@@ -8,7 +8,7 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
 	name: { type: String, required: true },
-	value: { type: Number, min: 0, required: [true, 'You need to specify a value for debt'] },
+	value: { type: Number, min: 0, required: true },
 	status: { type: String, required: false, uppercase: true,
 		enum: ['PAID', 'PENDING', 'SCHEDULED'] }
 });
